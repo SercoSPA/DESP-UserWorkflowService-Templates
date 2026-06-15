3b08be4a8d9861e0a3208881c58d9d20aeb26ca5
Andreas Bayer <bayer@geoville.com>
2026-06-15 16:13:59 +0200

    fix: removed notebook stream decoder from requirements and included .abi3.so for sentinel 3 notebooks

---
+++ DestineStreamer/nb_sentinel_3/notebook_stream_decoder_geohash/__init__.py
@@ -0,0 +1,5 @@
+from .notebook_stream_decoder_geohash import *
+
+__doc__ = notebook_stream_decoder_geohash.__doc__
+if hasattr(notebook_stream_decoder_geohash, "__all__"):
+    __all__ = notebook_stream_decoder_geohash.__all__

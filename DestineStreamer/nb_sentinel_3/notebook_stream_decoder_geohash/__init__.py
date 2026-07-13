58d60cd30727a35b44abd22e8dbec95d3508df2d
Andreas Bayer <bayer@geoville.com>
2026-07-13 10:13:16 +0200

    fix: removed the so and reinstated the url in requirements.txt

--- DestineStreamer/nb_sentinel_3/notebook_stream_decoder_geohash/__init__.py
+++
@@ -1,5 +0,0 @@
-from .notebook_stream_decoder_geohash import *
-
-__doc__ = notebook_stream_decoder_geohash.__doc__
-if hasattr(notebook_stream_decoder_geohash, "__all__"):
-    __all__ = notebook_stream_decoder_geohash.__all__
